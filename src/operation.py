9518dc55e2ab6b16e38ff9265d9fbc4c26c0a984
Gabry848 <famiglia.cipriani3@gmail.com>
2024-12-16 17:15:43 +0100

    risolti problemi con la build

--- src/operation.py
+++ src/operation.py
@@ -1,13 +1,21 @@
-import pathlib
-from pathlib import Path
 import json
 import shutil
 
-from rich import console
+import os
+import sys
+
+def get_base_path():
+    """Restituisce il percorso base del file corrente, anche con PyInstaller."""
+    if hasattr(sys, "_MEIPASS"):
+        # Quando il programma è eseguibile
+        return sys._MEIPASS
+    else:
+        # Quando è eseguito come script Python
+        return os.path.dirname(os.path.abspath(__file__))
 
 
 def read_file():
-    with open(pathlib.Path(__file__).parent / 'shared/path.json') as f:
+    with open('shared\\path.json') as f:
         return json.load(f)
     
     
@@ -16,10 +24,10 @@ def copy_files(data):
     
     for element in data:
         # Percorso del file da copiare
-        source = Path(element['path'])
+        source = element['path']
 
         # Nuovo percorso di destinazione
-        destination = Path(element['to'])
+        destination = element['to']
 
         # Copiare il file
         try:
@@ -30,13 +38,13 @@ def copy_files(data):
 def add_path(path, to, description):
     data = read_file()
     data.append({"path": path, "to": to, "description": description})
-    with open(pathlib.Path(__file__).parent / 'shared/path.json', 'w') as f:
+    with open('shared\\path.json', 'w') as f:
         json.dump(data, f, indent=4)
 
 def remove_path_id(id_):
     data = read_file()
     data.pop(int(id_))
-    with open(pathlib.Path(__file__).parent / 'shared/path.json', 'w') as f:
+    with open('shared\\path.json', 'w') as f:
         json.dump(data, f, indent=4)
         
 def remove_path(path):
@@ -49,7 +57,7 @@ def remove_path(path):
             break
         i += 1
     
-    with open(pathlib.Path(__file__).parent / 'shared/path.json', 'w') as f:
+    with open('shared\\path.json', 'w') as f:
         json.dump(data, f, indent=4)
         
 if __name__ == "__main__":
