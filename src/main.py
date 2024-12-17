4fc2a5102916b6d63bd8858445b7859842573717
Gabry848 <famiglia.cipriani3@gmail.com>
2024-12-17 18:05:01 +0100

    aggiunta del comando per cancellare i percorsi possibilita` di spstare cartelle, miglioramenti nel logging e aggiornamenti al file .gitignore

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import click
 from rich.console import Console
 from rich.table import Table
 
+import logging
+
 import operation
 
-# Crea un'istanza di Console di Rich per stampare in modo bello
+# Crea un'istanza di Console di Rich
 console = Console()
 
 @click.command()
@@ -47,18 +49,21 @@ def move_files():
     """copy the file to the new path """
     operation.copy_files(operation.read_file())
 
-
+def clear():
+    operation.clear()
+    
+    
 @click.group()
 def cli():
     """Programma CLI con Click e Rich"""
     pass
 
 # Aggiungi i comandi al gruppo CLI
-cli.add_command(table)
-cli.add_command(move_files)
-cli.add_command(add_path)
-cli.add_command(remove_path)
-
+cli.add_command(table, name="list")
+cli.add_command(move_files, name="start")
+cli.add_command(add_path, name="add")
+cli.add_command(remove_path, name="remove")
+cli.add_command(clear, name="clear")
 
 if __name__ == "__main__":
     cli()
--- src/operation.py
+++ src/operation.py
@@ -2,16 +2,14 @@ import json
 import shutil
 
 import os
-import sys
+import logging
+from rich.logging import RichHandler
 
-def get_base_path():
-    """Restituisce il percorso base del file corrente, anche con PyInstaller."""
-    if hasattr(sys, "_MEIPASS"):
-        # Quando il programma è eseguibile
-        return sys._MEIPASS
-    else:
-        # Quando è eseguito come script Python
-        return os.path.dirname(os.path.abspath(__file__))
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+log = logging.getLogger("rich")
 
 
 def read_file():
@@ -29,17 +27,36 @@ def copy_files(data):
         # Nuovo percorso di destinazione
         destination = element['to']
 
-        # Copiare il file
-        try:
+        if os.path.isfile(source):
             shutil.copy(source, destination)
-        except:
-            print("Errore nel copiare il file")
+            log.info("Copy file " + source + " to " + destination, extra={"markup": True})
+        elif os.path.isdir(source):
+            if os.path.exists(destination):
+                log.info(f"La cartella di destinazione '{destination}' esiste già.", extra={"markup": True})
+                # Opzione: sovrascrivi o unisci
+                log.warning("Vuoi sovrascrivere la cartella? (s/n):", extra={"markup": True})
+                risposta = input("                    ").lower()
+                if risposta == 's':
+                    shutil.rmtree(destination)  # Elimina la cartella di destinazione
+                    shutil.copytree(source, destination)
+                    log.info("Copy folder " + source + " to " + destination + " sovrascrivendo il contenuto.", extra={"markup": True})
+                else:
+                    log.info("operazione annullata.", extra={"markup": True})
+            else:
+                # Copia la cartella se la destinazione non esiste
+                shutil.copytree(source, destination)
+                log.info("Copy folder " + source + " to " + destination, extra={"markup": True})
+            
+        else:
+            log.error("PATH NOT FOUND", extra={"markup": True})
 
 def add_path(path, to, description):
     data = read_file()
     data.append({"path": path, "to": to, "description": description})
     with open('shared\\path.json', 'w') as f:
         json.dump(data, f, indent=4)
+    
+    log.info("Added path " + path, extra={"markup": True})
 
 def remove_path_id(id_):
     data = read_file()
@@ -59,6 +76,16 @@ def remove_path(path):
     
     with open('shared\\path.json', 'w') as f:
         json.dump(data, f, indent=4)
+
+    log.info("Remove path " + path, extra={"markup": True})    
+
+
+def clear():
+    data = []
+    with open('shared\\path.json', 'w') as f:
+        json.dump(data, f, indent=4)
         
+    log.info("Clear all paths", extra={"markup": True})
+    
 if __name__ == "__main__":
     copy_files(read_file())
